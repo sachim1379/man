04177b36c28fbab5483ceb0ba572e4ef2024a484
sachim1379 <sens@Laptop.com>
2021-09-16 11:32:46 -0400

    **linux code executor update**

---
+++ linuxCommandExecute.c
@@ -0,0 +1,16 @@
+//gcc -o [outputFileName] [inputSourceCodeName]
+//system() requires stdlib.h
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+printf("Code Executed\n");
+system("pwd");
+
+
+return 0;
+}
+
