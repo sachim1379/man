d0823edc80647b6525581da92d563c2909ed1e60
asdf <aaa>
2021-10-02 23:55:37 +0900

    automatic committed by PY-script

--- autogitall.py
+++ autogitall.py
@@ -1,7 +1,7 @@
 import os
 os.system("git add *")
 print("git add * EXECUTED")
-os.system("git commit -m 'automatic committed by PY-script'")
+os.system('git commit -m "automatic committed by PY-script"')
 print("git commit EXECUTED")
 os.system("git push")
 print("git push EXECUTED")
