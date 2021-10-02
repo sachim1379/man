964fcf4660aa2a56a0543bd93e80758b0c194728
yada <OstrichDesktop@gmail.com>
2021-10-03 00:16:21 +0900

    automatic committed by PY-script

--- git_auto_add_commit_push.py
+++ git_auto_add_commit_push.py
@@ -1,7 +1,8 @@
 import os
 os.system("git add *")
-print("git add * EXECUTED")
+print(":::git add * EXECUTED:::")
 os.system('git commit -m "automatic committed by PY-script"')
-print("git commit EXECUTED")
+print(":::git commit EXECUTED:::")
 os.system("git push")
-print("git push EXECUTED")
+print(":::git push EXECUTED:::")
+print(":::return:::")
